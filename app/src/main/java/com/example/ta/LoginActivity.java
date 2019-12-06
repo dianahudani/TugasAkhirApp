1be19f3d599fff5c03a0906fe97233fc97337237
dianahudani <dianakisyono@gmail.com>
2019-12-07 00:43:30 +0700

    API

--- app/src/main/java/com/example/ta/LoginActivity.java
+++ app/src/main/java/com/example/ta/LoginActivity.java
@@ -1,28 +1,97 @@
 package com.example.ta;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.ta.apihelper.BaseApiService;
+import com.example.ta.apihelper.UtilsApi;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LoginActivity extends AppCompatActivity {
 
+    EditText username, password;
     Button login;
+    ProgressDialog loading;
+    Context mContext;
+    BaseApiService mApiService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
         login = findViewById(R.id.loginbtn);
+        mContext = this;
+            mApiService = UtilsApi.getAPIService();
+            initComponents();
 
+
+    }
+
+    private void initComponents(EditText editText) {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.pass);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i2 = new Intent(LoginActivity.this, CameraActivity.class);
-                startActivity(i2);
+                loading = ProgressDialog.show(mContext, null, "Harap Tunggu..", true, false);
+                requestLogin();
             }
         });
+    }
+
+    private void requestLogin() {
+        mApiService.loginRequest(username.getText().toString(), password.getText().toString())
+                .enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if(response.isSuccessful()){
+                            loading.dismiss();
+                            try{
+                                JSONObject jsonRESULTS = new JSONObject(response.body().string());
+                                if(jsonRESULTS.getString("error").equals("false")){
+                                    Toast.makeText(mContext, "BERHASIL LOGIN", Toast.LENGTH_SHORT).show();;
+                                    String nama = jsonRESULTS.getJSONObject("user").getString("nama");
+                                    Intent intent = new Intent(mContext,CameraActivity.class)
+                                    intent.putExtra("result_nama", nama);
+                                    startActivity(intent);
+                                } else{
+                                    String error_message = jsonRESULTS.getString(("error_msg"));
+                                    Toast.makeText(mContext, error_message, Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else{
+                            loading.dismiss();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Log.e("debug", "onFailure: ERROR > " + t.toString());
+                        loading.dismiss();
+                    }
+                });
     }
 }
